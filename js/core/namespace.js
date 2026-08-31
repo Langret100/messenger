56ddf21a79564197ed8b6f56af1920532e4216e9
Langret100 <99009867+Langret100@users.noreply.github.com>
2026-08-31 14:42:55 +0900

    Delete js directory

--- js/core/namespace.js
+++
@@ -1,5 +0,0 @@
-/* 전역 노출은 MiniTalk 하나로 제한하고 하위 모듈이 같은 객체를 공유합니다. */
-window.MiniTalk = window.MiniTalk || {};
-MiniTalk.Features = MiniTalk.Features || {};
-MiniTalk.Tools = MiniTalk.Tools || {};
-MiniTalk.UI = MiniTalk.UI || {};
